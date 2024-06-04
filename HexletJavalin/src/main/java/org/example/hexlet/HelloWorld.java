b52e56f770444d18a5127ef42726b8cf455895dc
SpaceLudens <SpaceLuden1@gmail.com>
2024-06-04 12:07:06 +0300

    Independent work number 7

--- HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
+++ HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,20 +1,15 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
-import io.javalin.http.HttpStatus;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        // Создаем приложение
-        var app = Javalin.create(config -> {
-            config.bundledPlugins.enableDevLogging();
+        Javalin app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
+        app.get("/users/{id}/post/{postId}", ctx -> {
+            var userId = ctx.pathParam("id");
+            var postId =  ctx.pathParam("postId");
+            ctx.result("User ID: " + userId + " Post ID: " + postId);
         });
-        app.get("/users/{id}/post/{postId}", context -> {
-            var userId = context.pathParam("id");
-            var postId = context.pathParam("postId");
-
-            context.result("User ID: " + userId + ", Post ID: " + postId);
-        });
-        app.start(7070); // Стартуем веб-сервер
+        app.start(7070);
     }
 }
